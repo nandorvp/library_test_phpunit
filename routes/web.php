edfe1cf995cea31ccf77cdb93dc068b724bf988b
fernandomonteiro98 <nandorvp98@gmail.com>
2023-04-03 18:21:32 +0100

    working in progress for books resource

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthorsController;
 use App\Http\Controllers\BooksController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::post('/books',[BooksController::class, 'store']);
 Route::patch('/books/{book}',[BooksController::class, 'update']);
+Route::delete('/books/{book}',[BooksController::class, 'destroy']);
+
+Route::post('/authors', [AuthorsController::class, 'store']);
 
--- tests/Feature/BookReservationManagement.php
+++ tests/Feature/BookReservationManagement.php
@@ -6,21 +6,22 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\Book;
 use Tests\TestCase;
 
-class BookReservationTest extends TestCase
+class BookReservationManagement extends TestCase
 {
     use RefreshDatabase;
     /** @test */
     public function a_book_can_be_added_to_the_library()
     {
-        $this->withoutExceptionHandling();
 
         $response = $this->post('/books',[
             'title'=> 'Cool Book Title',
             'author' => 'Nando',
         ]);
 
-        $response->assertOK();
+        $book = Book::first();
+
         $this->assertCount(1, Book::all());
+        $response->assertRedirect($book->path());
 
     }
 
@@ -50,7 +51,6 @@ public function a_title_is_required() {
     /** @test  */
     public function a_book_can_be_updated()
     {
-        $this->withoutExceptionHandling();
 
          $this->post('/books',[
             'title'=> 'Cool Title',
@@ -66,5 +66,25 @@ public function a_title_is_required() {
 
         $this->assertEquals('New Title', Book::first()->title);
         $this->assertEquals('New Author', Book::first()->author);
+        $response->assertRedirect($book->path());
     }
+
+    /** @test */
+public function a_book_can_be_deleted() {
+
+    $this->post('/books',[
+        'title'=> 'Cool Title',
+        'author' => 'Nando',
+    ]);
+
+    $book = Book::first();
+    $this->assertCount(1,Book::all());
+//    A parte de cima não é necessária, apenas para realmente verificar que adiciona 1 livro e depois elimina. Mas no primeiro
+//    teste que fazemos já foi feita a verificação.
+
+    $response = $this->delete($book->path());
+
+    $this->assertCount(0,Book::all());
+    $response->assertRedirect('/books');
+}
 }
