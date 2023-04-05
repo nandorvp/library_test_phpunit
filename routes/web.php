634c5d00653ab309dc82660f76ba145ef121586e
fernandomonteiro98 <nandorvp98@gmail.com>
2023-04-05 16:31:10 +0100

    Authentication and Checkout and Return Feature Tests

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\AuthorsController;
 use App\Http\Controllers\BooksController;
+use App\Http\Controllers\ReturnBookController;
+use App\Http\Controllers\CheckoutBookController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,3 +17,11 @@ Route::delete('/books/{book}',[BooksController::class, 'destroy']);
 
 Route::post('/authors', [AuthorsController::class, 'store']);
 
+Route::post('/checkout/{book}', [CheckoutBookController::class, 'store']);
+Route::post('/return/{book}', [ReturnBookController::class, 'store']);
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
